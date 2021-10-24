fdaf6aa168c5ef121e8b27804d6c53d859ebf690
Ghaith Abdullateef Y Abdullateef <khojaghaith@gmail.com>
2021-10-24 23:19:15 +0000

    Changed music.py

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ for i in range(len(cogs)):
   cogs[i].setup(client)
 
 
-client.run('ODk1NDI5NzIyNzA2Njc3ODAx.YV4b6g.Tilxdc4zK-0aeCwid5amwGuXTaA')
+client.run('ODk1NDI5NzIyNzA2Njc3ODAx.YV4b6g.LIpXWRsMByq4Z260NeELu-bE5AE')
--- music.py
+++ music.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-import youtube_dl
+import yt_dlp
 
 class music(commands.Cog):
   def __init__(self, client):
@@ -22,12 +22,15 @@ class music(commands.Cog):
 
   @commands.command()
   async def play(self,ctx,url):
+    if ctx.voice_client is None:
+      await ctx.author.voice.channel.connect()
+
     ctx.voice_client.stop()
     FFMPEG_OPTIONS = {'options': '-vn'}  
     YDL_OPTIONS = {'format':'bestaudio'}
     vc = ctx.voice_client
 
-    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+    with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
       info = ydl.extract_info(url, download=False)
       url2 = info['formats'][0]['url']
       source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
