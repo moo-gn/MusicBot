f050f2ce68e2c716250dab95900ac68682d06116
Ghaith Abdullateef Y Abdullateef <khojaghaith@gmail.com>
2021-10-25 19:07:23 +0000

    changed prefix to "."

--- music_main.py
+++ music_main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import music
 
 cogs = [music]
-client = commands.Bot(command_prefix='$', intents = discord.Intents.all(), case_insensitive = True)
+client = commands.Bot(command_prefix='.', intents = discord.Intents.all(), case_insensitive = True)
 
 for i in range(len(cogs)):
   cogs[i].setup(client)
