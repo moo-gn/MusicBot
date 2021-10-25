a36bf2859fd88ea34d347e19c16f72e07f0e4557
Ghaith Abdullateef Y Abdullateef <khojaghaith@gmail.com>
2021-10-25 08:48:16 +0000

    Implemented queue and added embeded messages plus added skip

--- music.py
+++ music.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import yt_dlp
-from queueembed import queue_list
+import queueembed as qb
 from search_yt import search
 import asyncio
 
@@ -9,8 +9,10 @@ class music(commands.Cog):
   def __init__(self, client):
     self.client = client
     self.queue = []
+    self.play_status = False
+    self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
 
-  @commands.command()
+  @commands.command(aliases=['j'])
   async def join(self,ctx):
     if ctx.author.voice is None:
       await ctx.send('Please join a voice channel!')
@@ -26,61 +28,80 @@ class music(commands.Cog):
 
   def play_next(self, ctx):
     if len(self.queue) > 0:
-      fetch = self.queue.pop(0)  
+      fetch = self.queue.pop(0) 
+      ctx.voice_client.play(fetch[0], after= lambda x : self.play_next(ctx)) 
+      
 
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-      YDL_OPTIONS = {'format':'bestaudio'}
-
-      with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(fetch[0], download=False)
-        url2 = info['formats'][0]['url']
-        source = discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        ctx.voice_client.play(source, after= lambda x : self.play_next(ctx))
-
-  @commands.command()
+  @commands.command(aliases=['add'])
   async def play(self,ctx,*,message):
     await asyncio.sleep(1)
     fetch = search(message)
+    print(fetch[1])
 
     if ctx.voice_client is None:
       await ctx.author.voice.channel.connect()
+ 
+    YDL_OPTIONS = {'format':'bestaudio'}
 
-    if ctx.voice_client.is_playing():
-        self.queue.append([fetch[0], fetch[1]])
-    else:  
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 'options': '-vn'}
-      YDL_OPTIONS = {'format':'bestaudio'}
+    with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(fetch[0], download=False)
+      url2 = info['formats'][0]['url']
+      source = await discord.FFmpegOpusAudio.from_probe(url2, **self.FFMPEG_OPTIONS)
 
-      with yt_dlp.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(fetch[0], download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+      if ctx.voice_client.is_playing():
+        self.queue.append([source, fetch[1]])
+        await ctx.send(embed=qb.add_song_playing(fetch[1]))
+      else:  
         ctx.voice_client.play(source, after= lambda x : self.play_next(ctx))
+        self.play_status = True 
+        await ctx.send(embed=qb.first_song_playing(fetch[1]))
 
 
 
-  @commands.command()
+  @commands.command(aliases=['stop', 'hold'])
   async def pause(self,ctx):
-    await ctx.voice_client.pause()
+    if ctx.voice_client.is_playing() and self.play_status:
+        await ctx.voice_client.pause()
+        self.play_status = False
+        await ctx.send(embed=qb.send_msg('Paused music'))
+    else:
+      await ctx.send(embed=qb.send_msg('No music is playing'))
 
   @commands.command()
   async def list(self,ctx):
-    await ctx.send(embed=queue_list(self.queue)) 
+    await ctx.send(embed=qb.queue_list(self.queue)) 
 
-  @commands.command()
+  @commands.command(aliases=['continue'])
   async def resume(self,ctx):
-    await ctx.voice_client.resume()  
+    if not ctx.voice_client.is_playing() and not self.play_status:
+      await ctx.voice_client.resume() 
+      self.play_status = True 
+      await ctx.send(embed=qb.send_msg('Resume playing'))
+    else:
+      await ctx.send(embed=qb.send_msg('Music is already playing!'))
 
   @commands.command()
   async def remove(self,ctx,*,message):
     try:
-      self.queue.pop(int(message)-1)
+      fetch = self.queue.pop(int(message)-1)
+      await ctx.send(embed=qb.removed_song(fetch[1]))
     except IndexError: 
       await ctx.send("Index error")
 
-  @commands.command()
-  async def clear(self):
+  @commands.command(aliases=['clr'])
+  async def clear(self, ctx):
     self.queue.clear()
+    await ctx.send(embed=qb.send_msg('Cleared the queue!'))
+
+  @commands.command()
+  async def skip(self, ctx):
+    if ctx.voice_client.is_playing() and self.play_status:
+      await ctx.send(embed=qb.send_msg('Skipped!'))
+      await ctx.voice_client.stop()
+      self.play_next(ctx)
+    else:
+      await ctx.send(embed=qb.send_msg('Nothing is playing to skip!')) 
+     
 
 def setup(client):
   client.add_cog(music(client))    
--- queueembed.py
+++ queueembed.py
@@ -1,8 +1,26 @@
 import discord
 
+color_in = 0xADD8E6
+
 def queue_list(x):
-  listembed = discord.Embed(title = 'Song Queue:', color=0x0000FF)
+  listembed = discord.Embed(title = 'Song Queue:', color=color_in)
   listembed.set_thumbnail(url = 'https://cdn.discordapp.com/attachments/901624454688440430/901664414963499079/musicStops.png')
   for i in range(len(x)):
     listembed.add_field(name=f'{i + 1}.{x[i][1]}', value = '\u200b', inline=False)
   return listembed 
+
+def first_song_playing(x):
+  embed = discord.Embed(description =f'Playing {x}', color=color_in)
+  return embed
+
+def add_song_playing(x):
+  embed = discord.Embed(description = f'Added {x} to the queue', color=color_in)
+  return embed
+
+def removed_song(x):
+  embed = discord.Embed(description = f'Removed {x} from the queue', color=color_in)
+  return embed
+
+def send_msg(x):
+  embed = discord.Embed(description = x, color=color_in)
+  return embed   
