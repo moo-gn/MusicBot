ecb558aa157a3d3684c327ab2ebaccc4b56423bc
Ghaith Abdullateef Y Abdullateef <khojaghaith@gmail.com>
2021-10-28 07:01:18 +0000

    Reimplemented parsing using the HTML library

--- search_yt.py
+++ search_yt.py
@@ -1,39 +1,53 @@
 import requests
-import html
+from html.parser import HTMLParser
+
+
+class TitleParser(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.recording = 0
+    self.title = ''
+
+  def handle_starttag(self, tag, attributes):
+    if tag == 'title':
+      self.recording = 1
+
+  def handle_endtag(self, tag):
+    if tag == 'title' and self.recording:
+      self.recording -= 1
+
+  def handle_data(self, data):
+    if self.recording:
+      self.title = data
+
+class LinkParser(HTMLParser):
+  def __init__(self):
+    HTMLParser.__init__(self)
+    self.link = ''
+
+  def handle_data(self, data):
+    if data.find('watch?v=') != -1:
+      self.link = data[data.find('watch?v='):data.find('watch?v=') + 19]
+
 
 def search(query):
-    url = f"https://www.youtube.com/results?search_query={query}"
-    r = requests.get(url)
-    # Parse response
-    for idx, chr in enumerate(r.text):
-        if chr == "?":
-            if r.text[idx+1] == "v":
-              link = f"https://www.youtube.com/watch{r.text[idx:idx+14]}"
-              response = requests.get(link)
-              txt = response.text.split()
-              title = []
-              for idx, word in enumerate(txt):
-                if word == 'name="title"':
-                  i = 1
-                  word = txt[idx + i]
-                  while word != 'name="description"':
-                    title.append(word)
-                    i += 1
-                    word = txt[idx + i]
-                  break  
 
-              title = " ".join(title)
-              title = title[9:-7]
-              title = html.unescape(title)
-              return[link, title]
+  #Fetch the HTML of the search page
+  url = f"https://www.youtube.com/results?search_query={query}"
+  response = requests.get(url)
 
-    r = requests.get(url)
+  #Parse it for the first suggestion link
+  parser = LinkParser()
+  parser.feed(response.text)
+  link = f"https://www.youtube.com/{parser.link}"
 
-    # Parse response
-    for idx, chr in enumerate(r.text):
-        if chr == "?":
-            if r.text[idx+1] == "v":
-                return [f"https://www.youtube.com/watch{r.text[idx:idx+14]}", "video_title"]
+  #Prase the suggestion link for the title
+  response = requests.get(link)
+  parser = TitleParser()
+  parser.feed(response.text)
+  title = parser.title
+
+  return[link, title]
 
 if __name__ == "__main__":
     # pass
