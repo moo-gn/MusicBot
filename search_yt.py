30d4e7d10e0598dfe4c8fc48b8ae6af82fc651fb
zuaird <demx13@gmail.com>
2022-03-02 22:19:23 +0000

    Revert "removed audio keyword from search"
    This reverts commit 861a9a5ffb4ab3bced7b8384a2541674a1d61891.

--- search_yt.py
+++ search_yt.py
@@ -33,6 +33,8 @@ class LinkParser(HTMLParser):
 
 def search(query):
 
+  #Add rule to check for audio
+  query = query + ' Audio'
   #encode to URL safe 
   query = urllib.parse.quote_plus(query)
 
